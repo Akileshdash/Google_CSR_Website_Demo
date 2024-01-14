6306a9f66f6857774db23ef214b4414923f88bbc
Akileshdash <119668304+Akileshdash@users.noreply.github.com>
2024-01-14 17:41:44 +0530

    Header Responsiveness Checked

--- script.js
+++ script.js
@@ -2,60 +2,60 @@
 
 // ---------Responsive-navbar-active-animation-----------
 function test(){
-    var tabsNewAnim = $('#navbarSupportedContent');
-    var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
-    var activeItemNewAnim = tabsNewAnim.find('.active');
-    var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-    var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-    var itemPosNewAnimTop = activeItemNewAnim.position();
-    var itemPosNewAnimLeft = activeItemNewAnim.position();
-    $(".hori-selector").css({
-        "top":itemPosNewAnimTop.top + "px", 
-        "left":itemPosNewAnimLeft.left + "px",
-        "height": activeWidthNewAnimHeight + "px",
-        "width": activeWidthNewAnimWidth + "px"
-    });
-    $("#navbarSupportedContent").on("click","li",function(e){
-        $('#navbarSupportedContent ul li').removeClass("active");
-        $(this).addClass('active');
-        var activeWidthNewAnimHeight = $(this).innerHeight();
-        var activeWidthNewAnimWidth = $(this).innerWidth();
-        var itemPosNewAnimTop = $(this).position();
-        var itemPosNewAnimLeft = $(this).position();
-        $(".hori-selector").css({
-            "top":itemPosNewAnimTop.top + "px", 
-            "left":itemPosNewAnimLeft.left + "px",
-            "height": activeWidthNewAnimHeight + "px",
-            "width": activeWidthNewAnimWidth + "px"
-        });
-    });
+  var tabsNewAnim = $('#navbarSupportedContent');
+  var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
+  var activeItemNewAnim = tabsNewAnim.find('.active');
+  var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+  var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+  var itemPosNewAnimTop = activeItemNewAnim.position();
+  var itemPosNewAnimLeft = activeItemNewAnim.position();
+  $(".hori-selector").css({
+      "top":itemPosNewAnimTop.top + "px", 
+      "left":itemPosNewAnimLeft.left + "px",
+      "height": activeWidthNewAnimHeight + "px",
+      "width": activeWidthNewAnimWidth + "px"
+  });
+  $("#navbarSupportedContent").on("click","li",function(e){
+      $('#navbarSupportedContent ul li').removeClass("active");
+      $(this).addClass('active');
+      var activeWidthNewAnimHeight = $(this).innerHeight();
+      var activeWidthNewAnimWidth = $(this).innerWidth();
+      var itemPosNewAnimTop = $(this).position();
+      var itemPosNewAnimLeft = $(this).position();
+      $(".hori-selector").css({
+          "top":itemPosNewAnimTop.top + "px", 
+          "left":itemPosNewAnimLeft.left + "px",
+          "height": activeWidthNewAnimHeight + "px",
+          "width": activeWidthNewAnimWidth + "px"
+      });
+  });
 }
 $(document).ready(function(){
-    setTimeout(function(){ test(); });
+  setTimeout(function(){ test(); });
 });
 $(window).on('resize', function(){
-    setTimeout(function(){ test(); }, 500);
+  setTimeout(function(){ test(); }, 500);
 });
 $(".navbar-toggler").click(function(){
-    $(".navbar-collapse").slideToggle(300);
-    setTimeout(function(){ test(); });
+  $(".navbar-collapse").slideToggle(300);
+  setTimeout(function(){ test(); });
 });
 
 
 
 // --------------add active class-on another-page move----------
 jQuery(document).ready(function($){
-    // Get current path and find target link
-    var path = window.location.pathname.split("/").pop();
+  // Get current path and find target link
+  var path = window.location.pathname.split("/").pop();
 
-    // Account for home page with empty path
-    if ( path == '' ) {
-        path = 'index.html';
-    }
+  // Account for home page with empty path
+  if ( path == '' ) {
+      path = 'index.html';
+  }
 
-    var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
-    // Add active class to target link
-    target.parent().addClass('active');
+  var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
+  // Add active class to target link
+  target.parent().addClass('active');
 });
 
 
@@ -82,78 +82,78 @@ var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
-    if (window.scrollY >= sticky) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    }
+  if (window.scrollY >= sticky) {
+      navbar.classList.add("sticky");
+  } else {
+      navbar.classList.remove("sticky");
+  }
 }
 
 // script.js
 
 $(document).ready(function() {
-    // Smooth scrolling for anchor links
-    $('a[href^="#"]').on('click', function(event) {
-      event.preventDefault();
-  
-      var target = $(this.getAttribute('href'));
-      if (target.length) {
-        $('html, body').stop().animate({
-          scrollTop: target.offset().top
-        }, 1000); // Adjust the duration of the scroll animation as needed
-      }
-    });
+  // Smooth scrolling for anchor links
+  $('a[href^="#"]').on('click', function(event) {
+    event.preventDefault();
+
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top
+      }, 1000); // Adjust the duration of the scroll animation as needed
+    }
   });
-  
-  jQuery.fn.liScroll = function(settings) {
-	settings = jQuery.extend({
-		travelocity: 0.03
-		}, settings);		
-		return this.each(function(){
-				var $strip = jQuery(this);
-				$strip.addClass("newsticker")
-				var stripHeight = 1;
-				$strip.find("li").each(function(i){
-					stripHeight += jQuery(this, i).outerHeight(true); // thanks to Michael Haszprunar and Fabien Volpi
-				});
-				var $mask = $strip.wrap("<div class='mask'></div>");
-				var $tickercontainer = $strip.parent().wrap("<div class='tickercontainer'></div>");								
-				var containerHeight = $strip.parent().parent().height();	//a.k.a. 'mask' width 	
-				$strip.height(stripHeight);			
-				var totalTravel = stripHeight;
-				var defTiming = totalTravel/settings.travelocity;	// thanks to Scott Waye		
-				function scrollnews(spazio, tempo){
-				$strip.animate({top: '-='+ spazio}, tempo, "linear", function(){$strip.css("top", containerHeight); scrollnews(totalTravel, defTiming);});
-				}
-				scrollnews(totalTravel, defTiming);				
-				$strip.hover(function(){
-				  jQuery(this).stop();
-				},
-				function(){
-				  var offset = jQuery(this).offset();
-				  var residualSpace = offset.top + stripHeight;
-				  var residualTime = residualSpace/settings.travelocity;
-				  scrollnews(residualSpace, residualTime);
-				});			
-		});	
+});
+
+jQuery.fn.liScroll = function(settings) {
+settings = jQuery.extend({
+  travelocity: 0.03
+  }, settings);		
+  return this.each(function(){
+      var $strip = jQuery(this);
+      $strip.addClass("newsticker")
+      var stripHeight = 1;
+      $strip.find("li").each(function(i){
+        stripHeight += jQuery(this, i).outerHeight(true); // thanks to Michael Haszprunar and Fabien Volpi
+      });
+      var $mask = $strip.wrap("<div class='mask'></div>");
+      var $tickercontainer = $strip.parent().wrap("<div class='tickercontainer'></div>");								
+      var containerHeight = $strip.parent().parent().height();	//a.k.a. 'mask' width 	
+      $strip.height(stripHeight);			
+      var totalTravel = stripHeight;
+      var defTiming = totalTravel/settings.travelocity;	// thanks to Scott Waye		
+      function scrollnews(spazio, tempo){
+      $strip.animate({top: '-='+ spazio}, tempo, "linear", function(){$strip.css("top", containerHeight); scrollnews(totalTravel, defTiming);});
+      }
+      scrollnews(totalTravel, defTiming);				
+      $strip.hover(function(){
+        jQuery(this).stop();
+      },
+      function(){
+        var offset = jQuery(this).offset();
+        var residualSpace = offset.top + stripHeight;
+        var residualTime = residualSpace/settings.travelocity;
+        scrollnews(residualSpace, residualTime);
+      });			
+  });	
 };
 
 $(function(){
-    $("ul#ticker01").liScroll();
+  $("ul#ticker01").liScroll();
 });
 
 const items = document.querySelectorAll(".accordion button");
 
 function toggleAccordion() {
-  const itemToggle = this.getAttribute('aria-expanded');
-  
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
-  }
-  
-  if (itemToggle == 'false') {
-    this.setAttribute('aria-expanded', 'true');
-  }
+const itemToggle = this.getAttribute('aria-expanded');
+
+for (i = 0; i < items.length; i++) {
+  items[i].setAttribute('aria-expanded', 'false');
+}
+
+if (itemToggle == 'false') {
+  this.setAttribute('aria-expanded', 'true');
+}
 }
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
